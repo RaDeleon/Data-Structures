acb137be217e782b3ef3609f3809f85689cb9295
Radeleon <rudy209@comcast.net>
2019-07-23 13:50:34 -0700

    Made queue.py alot better

--- queue/queue.py
+++ queue/queue.py
@@ -1,9 +1,14 @@
+import sys
+
 class Queue:
   def __init__(self):
     self.size = 0
     # what data structure should we
     # use to store queue elements?
     self.storage = []
+
+  def isEmpty(self):
+        return self.storage == []
 
   def enqueue(self, item):
     self.storage.append(item)
@@ -14,7 +19,19 @@ class Queue:
       self.storage.pop(0)
       return sec_store
 
-  def len(self): 
+  def len(self):    
     return len(self.storage)
+    
+  def printQueue(self):
+        for items in self.storage:
+              print(items)
 
-    # test
+
+q = Queue()
+print(q.isEmpty())
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.printQueue()
+        
